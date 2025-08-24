e6793ee8d4b2f73002b2ee7b1e8d8b7191c6ba92
Sidiq Muhammad Toha <sidiq.muhamadtoha@enkripa.id>
2025-08-24 08:41:23 +0700

    feat: golang v1.0.0

--- api/index.go
+++ api/index.go
@@ -1,4 +1,3 @@
-// api/index.go
 package main
 
 import (
@@ -14,33 +13,30 @@ import (
 	"absensi/internal/http/router"
 )
 
-var (
-	once  sync.Once
-	app   http.Handler
-	sqlDB *sql.DB
-)
+var once sync.Once
+var app http.Handler
+var sqlDB *sql.DB
 
 func initApp() {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		// Di Vercel, pakai env di dashboard/CLI, jangan .env file
 		panic("DATABASE_URL is not set")
 	}
-	// koneksi pool kecil supaya ramah serverless
+
 	var err error
 	sqlDB, err = db.Connect(dsn)
 	if err != nil {
 		panic(err)
 	}
+
 	sqlDB.SetMaxOpenConns(5)
 	sqlDB.SetMaxIdleConns(2)
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
 
-	app = router.New(sqlDB) // router kamu sudah return http.Handler
+	app = router.New(sqlDB)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	once.Do(initApp)
-	// Semua route /api/* bakal jatuh ke mux milik kamu
 	app.ServeHTTP(w, r)
 }
